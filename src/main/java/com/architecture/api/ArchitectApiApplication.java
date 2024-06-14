e80aa8ef6a4eca6ef8c65807ef153d3f1e3b9c65
jwlee <dannis1124@naver.com>
2024-06-15 01:25:35 +0900

    initial

---
+++ src/main/java/com/architecture/api/ArchitectApiApplication.java
@@ -0,0 +1,13 @@
+package com.architecture.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/architecture/api/ArchitectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architecture.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
